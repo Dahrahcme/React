f0f1125d5f17a5b702754dc553c717149fedbdf3
Dahrah <tripleo4real@gmail.com>
2023-06-19 15:53:31 +0100

    Updated

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import NavBar from './Components/Navbar/NavBar';
-import Homepage from './Components/Homepage/Homepage';
-import About from './Components/About/About';
-import Blog from './Components/Blog/Blog';
-import Contact from './Components/Contact/Contact';
-import Services from './Components/Services/Services';
+import Homepage from './Components/Layout/Homepage/Homepage';
+import About from './Components/Layout/About/About';
+import Blog from './Components/Layout/Blog/Blog';
+import Contact from './Components/Layout/Contact/Contact';
+import Services from './Components/Layout/Services/Services';
+import Products from './Components/Layout/Products/Products';
 
 const App = () => {
   return (
@@ -13,14 +14,60 @@ const App = () => {
       <NavBar/>
       <Routes>
         <Route path="/Homepage" element={<Homepage />} />
+        <Route path="/Products" element={<Products />} />
         <Route path="/About" element={<About />} />
         <Route path="/Blog" element={<Blog />} />
         <Route path="/Contact" element={<Contact />} />
-        <Route path="/Services" element={<Services />} />
-      </Routes>
+        <Route path="/Services" element={<Services />} /> 
+      </Routes> 
     </div>    
   );
 }
 
 export default App;
  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
